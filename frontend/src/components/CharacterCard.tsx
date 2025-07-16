38fbe6a178fdc2a3c43cb58e5fc2764342f56a2b
Lennart Kuyvenhoven <lvkuyvenhoven@hotmail.com>
2025-07-16 20:12:19 +0200

    feat: Added pagination to the list

--- frontend/src/components/CharacterCard.tsx
+++ frontend/src/components/CharacterCard.tsx
@@ -1,11 +1,12 @@
-import { Button, Dialog, DialogActions, DialogContent, DialogTitle, Stack, Typography } from "@mui/material";
+import { Box, Button, Dialog, DialogActions, DialogContent, DialogTitle, Stack, Typography } from "@mui/material";
 import { useState } from "react";
 
 interface Props {
     characterName: string
+    characterUrl: string
 }
 
-export const CharacterCard = ({characterName}: Props) => {
+export const CharacterCard = ({characterName, characterUrl}: Props) => {
     const [showDialog, setShowDialog] = useState(false)
     
     const openDialog = () =>{
@@ -26,6 +27,21 @@ export const CharacterCard = ({characterName}: Props) => {
             <Stack gap={3}>
                 Some content
             </Stack>
+                {/* <img
+        src={characterUrl}
+        loading="lazy"
+      /> */}
+      <Box
+  component="img"
+  sx={{
+    height: 233,
+    width: 350,
+    maxHeight: { xs: 233, md: 167 },
+    maxWidth: { xs: 350, md: 250 },
+  }}
+  alt="The house from the offer."
+  src={characterUrl}
+/>
          </DialogContent>
          <DialogActions>
             <Button variant="outlined" onClick={closeDialog}>Close</Button>
--- frontend/src/pages/CharactersPage.tsx
+++ frontend/src/pages/CharactersPage.tsx
@@ -1,23 +1,19 @@
-import { Box, Typography } from "@mui/material"
+import { Box, Grid, Pagination, Stack, Typography } from "@mui/material"
 import { CharacterCard } from "../components/CharacterCard"
 // import { getCharacters } from "../services.ts/api"
 import { getCharacters } from 'rickmortyapi'
 import { useEffect, useState } from "react"
 import type { ApiResponse, Info, Character } from "rickmortyapi"
 
-interface CharacterEntry {
-  name: string,
-  id: number,
-  species: string
-}
-
 export const CharactersPage = () => {
-  const[characters, setCharacters] = useState<ApiResponse<Info<Character[]>>>([])
+  const[characters, setCharacters] = useState<ApiResponse<Info<Character[]>>>()
+
+    const [currentPage, setCurrentPage] = useState(0)
 
       const loadCharacters = async () => {
     try {
       // const allCharacters = await getCharacters()
-      const allCharacters = await getCharacters()
+      const allCharacters = await getCharacters({ page: currentPage })
       if (allCharacters.data.results)
       {
       setCharacters(allCharacters)
@@ -30,17 +26,29 @@ export const CharactersPage = () => {
 
   useEffect(() => {
     loadCharacters()
-  }, [])
+  }, [currentPage])
 
-    console.log("characters")
-  console.log(characters)
     
+  const selectPage = (event: React.ChangeEvent<unknown>, page: number) => {
+    event.preventDefault()
+    setCurrentPage(page)
+  }
+
   return (
     <Box>
+                    <Stack direction="column" gap={3}>
               <Typography variant="h3">Characters</Typography>
+
+<Grid container spacing={2} columns={8}>
         { characters?.data?.results?.length && characters.data.results.length > 0 && characters.data.results.map((character) => {
-            return <CharacterCard characterName={character.name} key={character.id}/>
+            return     (<Grid size={4}>
+              <CharacterCard characterName={character.name} characterUrl={character.image} key={character.id}/>
+                  </Grid>)            
         })}
+
+  </Grid>
+        <Pagination defaultPage={1} count={20} onChange={selectPage} variant="outlined" shape="rounded" color="secondary" />
+                      </Stack>
     </Box>
   )
 }
